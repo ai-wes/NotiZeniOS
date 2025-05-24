f1ef91cbf46dccd81980f9a6052ec172a570f21e
AI-Wes <132653112+ai-wes@users.noreply.github.com>
2025-05-24 12:20:00 -0700

    Initial Commit

---
+++ NotiZeniOS/NotiZeniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiZeniOSApp.swift
+//  NotiZeniOS
+//
+//  Created by Wesley Lagarde on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NotiZeniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
