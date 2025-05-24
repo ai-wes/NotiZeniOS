a990f1aa3eee894076e3c77cfcb6c5848fcd265d
AI-Wes <132653112+ai-wes@users.noreply.github.com>
2025-05-24 14:53:30 -0700

    Update

---
+++ NotiZenWatch Watch App/NotiZenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiZenWatchApp.swift
+//  NotiZenWatch Watch App
+//
+//  Created by Wesley Lagarde on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NotiZenWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
